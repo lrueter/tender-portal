02d9cf0d984704f8b395f04f8fc40785cf4488e5
lrueter <lars.rueter@gmail.com>
2025-04-18 21:49:05 +0800

    Add logout button in AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import { Container, Paper, Typography, Box } from '@mui/material';
+import { Container, Paper, Typography, Box, AppBar, Toolbar, Button } from '@mui/material';
+import { useClerk } from '@clerk/clerk-react';
 import DocumentationSection from './components/DocumentationSection';
 import QuoteUploadSection from './components/QuoteUploadSection';
 import AuthWrapper from './components/AuthWrapper';
 
 function App() {
+  const { signOut } = useClerk();
+
+  const handleSignOut = () => {
+    signOut();
+  };
+
   return (
     <AuthWrapper>
-      <Container maxWidth="lg" sx={{ py: 4 }}>
-        <Typography variant="h3" component="h1" gutterBottom align="center">
-          Trade Tender Portal
-        </Typography>
-        
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-          <Paper sx={{ p: 3 }}>
-            <Typography variant="h5" component="h2" gutterBottom>
-              Documentation
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ mb: 3 }}>
+          <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <Typography variant="h6">
+              Trade Tender Portal
             </Typography>
-            <DocumentationSection />
-          </Paper>
+            <Button color="inherit" onClick={handleSignOut}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
 
-          <Paper sx={{ p: 3 }}>
-            <Typography variant="h5" component="h2" gutterBottom>
-              Quote Submission
-            </Typography>
-            <QuoteUploadSection />
-          </Paper>
-        </Box>
-      </Container>
+        <Container maxWidth="lg" sx={{ py: 4 }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+            <Paper sx={{ p: 3 }}>
+              <Typography variant="h5" component="h2" gutterBottom>
+                Documentation
+              </Typography>
+              <DocumentationSection />
+            </Paper>
+
+            <Paper sx={{ p: 3 }}>
+              <Typography variant="h5" component="h2" gutterBottom>
+                Quote Submission
+              </Typography>
+              <QuoteUploadSection />
+            </Paper>
+          </Box>
+        </Container>
+      </Box>
     </AuthWrapper>
   );
 }
